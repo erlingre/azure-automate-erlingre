0687ef649e65cdc6b48acc7c2dadaf8401dccf7f
Erling Ringen Elvsrud <erling.ringen.elvsrud@nav.no>
2021-11-16 15:46:29 +0100

    Oppgave 3

---
+++ src/oppgave_3.ps1
@@ -0,0 +1,28 @@
+$ErrorActionPreference = 'Stop'
+
+$webrequest = Invoke-WebRequest -Uri http://nav-deckofcards.herokuapp.com/shuffle
+$kortstokkJson = $webRequest.Content
+
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+#foreach ($kort in $kortstokk) {
+#    Write-Output $kort
+#}
+#foreach ($kort in $kortstokk) {
+#    Write-Output "$($kort.suit[0])+$($kort.value)"
+#}
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + ","
+    }
+    return $streng.TrimEnd(',')
+}
+
+Write-Output "Kortstokk: $(kortstokkTilStreng -kortstokk $kortstokk)"
